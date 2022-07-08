e7305df910ddce80202884204f6604b46e76c642
Hudson Couto <hudson.sama@gmail.com>
2022-07-08 17:13:31 -0300

    create entry point of script

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
-FILE = "#{__dir__}/data/qgames.log"
+require 'json'
+require_relative './lib/parser/parser'
+require_relative './lib/report/report'
+
+FILE = "#{__dir__}/data/qgames.log".freeze
 
 class App
   def read_file
     file = File.open(FILE)
-    puts "Number of lines #{file.readlines.size}"
+    parsed_file = Lib::Parser::Parser.new(file:).parser
+    report = Lib::Report::Report.new(matches: parsed_file).default
+    File.write('report.json', report)
   end
 end
 
